0ee921af354844ae96ec6a47ed0c5365c7d36861
Alessandro Polidori <alessandro.polidori@gmail.com>
2011-04-15 11:41:39 +0200

    add new testProfiler.js for test profiler.js

--- testProfiler.js
+++ testProfiler.js
@@ -1,47 +1,76 @@
+var args = require('./lib/argparser.js').parse()
 var proReq = require("./profiler.js");
-var exten = "500";
 
-console.log("TEST with exten = " + exten);
+var l = Object.keys(args).length;
+
+if(l < 2)
+ help();
+
+var exten = args["-e"];
+var search = args["-s"];
+if(search == undefined)
+	search = "";
 
 var profiler = new proReq.Profiler();
-console.log("Profiler object created");
 
-/*
-// print all user profiles
-console.log("\nTEST printUserProfiles");
-profiler.printUserProfiles();
-console.log("\n");
+switch ( args["-t"] )
+{
+	case "initProfiles":
+		console.log("\nTesting initProfiles()");
+		break;
+	
+	case "checkActionCallOutPermit":
+                console.log("\nTesting checkActionCallOutPermit(exten): extent " + exten);
+                var res = profiler.checkActionCallOutPermit(exten);
+                console.log(res);
+                break;
 
-//
-console.log("\nTEST getUserProfile");
-var userProfile = profiler.getUserProfile(exten);
-console.log("RES:");
-console.log(userProfile);
-console.log("\n");
+	case "checkActionPhonebookPermit":
+                console.log("\nTesting checkActionPhonebookPermit(exten): extent " + exten);
+                var res = profiler.checkActionPhonebookPermit(exten);
+                console.log(res);
+                break;
 
-// print user category name
-console.log("\nTEST getUserCategory");
-var categoryName = profiler.getUserCategory(exten);
-console.log("RES:");
-console.log(categoryName);
-console.log("\n");
+	case "checkActionCallInPermit":
+                console.log("\nTesting checkActionCallInPermit(exten): extent " + exten);
+                var res = profiler.checkActionCallInPermit(exten);
+                console.log(res);
+                break;
+	
+	case "checkActionRedirectPermit":
+                console.log("\nTesting checkActionCallInPermit(exten): extent " + exten);
+                var res = profiler.checkActionRedirectPermit(exten);
+                console.log(res);
+                break;
 
-// print user permit actions
-console.log("\nTEST getUserPermitActions");
-var userPermitActions = profiler.getUserPermitActions(exten)
-console.log("RES:");
-console.log(userPermitActions);
-console.log("\n");
+	case "checkActionRecordPermit":
+                console.log("\nTesting checkActionRecordPermit(exten): extent " + exten);
+                var res = profiler.checkActionRecordPermit(exten);
+                console.log(res);
+                break;
 
-// print user deny actions
-console.log("\nTEST getUserDenyActions");
-var userDenyActions = profiler.getUserDenyActions(exten) 
-console.log("RES:");
-console.log(userDenyActions);
-console.log("\n");
-*/
+	case "checkActionHistoryCallPermit":
+                console.log("\nTesting checkActionHistoryCallPermit(exten): extent " + exten);
+                var res = profiler.checkActionHistoryCallPermit(exten);
+                console.log(res);
+                break;
 
-//
-console.log("TEST testPermitActionUser");
-console.log(profiler.testPermitActionUser(exten, "call_out"));
+
+	default:
+		help();
+}
+
+
+function help()
+{
+	console.log("Usage: node testProfiler.js -t <initProfiles|checkActionCallOutPermit|checkActionPhonebookPermit|checkActionCallInPermit|checkActionRedirectPermit|checkActionRecordPermit|checkActionHistoryCallPermit> -e <exten> [-s <search>] ");
+	process.exit(1);
+}
+
+function printResult(result)
+{
+	console.log("Result=\n");
+	console.log(result);
+	console.log("\n");
+};
 
