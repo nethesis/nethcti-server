ac9054fce3de93337aade5be0e687adb52de2a87
Alessandro Polidori <alessandro.polidori@gmail.com>
2011-05-02 12:03:20 +0200

    add test file 'testModop.js' for modop.js

---
+++ testModop.js
@@ -0,0 +1,45 @@
+var args = require('./lib/argparser.js').parse()
+var modopReq = require("./modop.js");
+
+var l = Object.keys(args).length;
+
+if(l < 2)
+ help();
+
+var exten = args["-e"];
+var search = args["-s"];
+if(search == undefined)
+	search = "";
+
+var modop = new modopReq.Modop();
+
+switch ( args["-t"] )
+{
+	case "constructor":
+		console.log("\nTesting initProfiles()");
+		break;
+	
+	case "checkActionCallOutPermit":
+                console.log("\nTesting checkActionCallOutPermit(exten): extent " + exten);
+                var res = profiler.checkActionCallOutPermit(exten);
+                console.log(res);
+                break;
+
+	default:
+		help();
+}
+
+
+function help()
+{
+	console.log("Usage: node testProfiler.js -t <constructor> -e <exten> [-s <search>] ");
+	process.exit(1);
+}
+
+function printResult(result)
+{
+	console.log("Result=\n");
+	console.log(result);
+	console.log("\n");
+};
+
