eadaf8f7ccece3e143d4dc14ee3d8c231b8aa551
Alessandro Polidori <alessandro.polidori@gmail.com>
2011-06-23 16:34:01 +0200

    proxycti: fix bug in logger.warn in hangup event

--- proxycti.js
+++ proxycti.js
@@ -818,7 +818,7 @@ am.addListener('hangup', function(headers) {
 			logger.info("removed callConnectedUniqueid '" + headers.uniqueid + "' from trunk '" + trunkTypeExt + "'")
 			updateAllClientsForOpWithTypeExt(trunkTypeExt)
 		} else
-			logger.warn("callConnected uniqueid '" + headers.uniqueid1 + "' has already not present into trunk '" + trunkTypeext  + "'")
+			logger.warn("callConnected uniqueid '" + headers.uniqueid1 + "' has already not present into trunk '" + trunkTypeExt + "'")
 	}
 
 	// ext
