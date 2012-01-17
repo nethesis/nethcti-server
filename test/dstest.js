2db5f16e39afffd87858742fbd269dca68b0c6f7
Alessandro Polidori <alessandro.polidori@gmail.com>
2012-01-17 15:15:24 +0100

    dstest: change name of 2 functions

--- test/dstest.js
+++ test/dstest.js
@@ -5,7 +5,7 @@ var dataCollector = new datareq.DataCollector();
 switch(args[0]){
 	case 'phonebook':
 		var content = args[1];
-		content===undefined ? printHelp() : '';
+		content===undefined ? help() : '';
 		console.log("PRIMA");
 		dataCollector.getContactsPhonebook(content,cbPhonebook);
 		console.log("DOPO");
@@ -13,23 +13,23 @@ switch(args[0]){
 	case 'cc':
 		var typecc = args[1];
 		var content = args[2];
-		(typecc===undefined || content===undefined) ? printHelp() : '';
+		(typecc===undefined || content===undefined) ? help() : '';
 		dataCollector.getCustomerCard(content,typecc,cbCC);
 	break;
 	case 'hist_call_curr_week':
 		var ext = args[1];
-		ext===undefined ? printHelp() : '';
+		ext===undefined ? help() : '';
 		dataCollector.getCurrentWeekHistoryCall(ext,cbHistCall);
 	break;
 	case 'hist_call_curr_month':
 		var ext = args[1];
-		ext===undefined ? printHelp() : '';
+		ext===undefined ? help() : '';
 		dataCollector.getCurrentMonthHistoryCall(ext,cbHistCallCurrMonth);
 	break;
 	case 'hist_call_day':
 		var ext = args[1];
 		var datex = args[2];
-		(ext===undefined || datex===undefined) ? printHelp() : '';
+		(ext===undefined || datex===undefined) ? help() : '';
 		dataCollector.getDayHistoryCall(ext,datex,cbHistCall);
 	break;
 	case 'ini':
@@ -37,7 +37,7 @@ switch(args[0]){
 		printObj(queries,'ini file:',true);
 	break;
 	default:
-		printHelp();
+		help();
 	break;
 }
 function cbHistCall(result){
@@ -45,7 +45,7 @@ function cbHistCall(result){
 }
 function cbHistCallCurrMonth(result){
 	printArr(result,'Results:',false);
-	printHelpExit();
+	helpExit();
 }
 function cbCC(result){
 	printArr(result,'Results:',true);
@@ -53,7 +53,7 @@ function cbCC(result){
 function cbPhonebook(result){
 	printArr(result,'Results:',true);
 }
-function printHelpExit(){
+function helpExit(){
 	console.log("CTRL+C to exit");
 }
 function printObj(obj,title,exit){
@@ -72,7 +72,7 @@ function printArr(arr,title,exit){
 		process.exit(0);
 	}
 }
-function printHelp(){
+function help(){
 	var cmd = allargs[1].split('/');
 	cmd = cmd[cmd.length-1];
 	console.log(cmd);
