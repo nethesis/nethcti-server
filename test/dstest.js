1f5a557b35455cf08b818805138e15f8a00a4e8f
Alessandro Polidori <alessandro.polidori@gmail.com>
2012-01-17 16:29:54 +0100

    add test of history at test/dstest

--- test/dstest.js
+++ test/dstest.js
@@ -16,6 +16,12 @@ switch(args[0]){
 		(typecc===undefined || content===undefined) ? help() : '';
 		dataCollector.getCustomerCard(content,typecc,cbCC);
 	break;
+	case 'hist_call_day':
+		var ext = args[1];
+		var datex = args[2];
+		(ext===undefined || datex===undefined) ? help() : '';
+		dataCollector.getDayHistoryCall(ext,datex,cbHistCall);
+	break;
 	case 'hist_call_curr_week':
 		var ext = args[1];
 		ext===undefined ? help() : '';
@@ -26,11 +32,37 @@ switch(args[0]){
 		ext===undefined ? help() : '';
 		dataCollector.getCurrentMonthHistoryCall(ext,cbHistCallCurrMonth);
 	break;
-	case 'hist_call_day':
+	case 'hist_sms_day':
 		var ext = args[1];
 		var datex = args[2];
 		(ext===undefined || datex===undefined) ? help() : '';
-		dataCollector.getDayHistoryCall(ext,datex,cbHistCall);
+		dataCollector.getDayHistorySms(ext,datex,cbHistSms);
+	break;
+	case 'hist_sms_curr_week':
+		var ext = args[1];
+		ext===undefined ? help() : '';
+		dataCollector.getCurrentWeekHistorySms(ext,cbHistSms);
+	break;
+	case 'hist_sms_curr_month':
+		var ext = args[1];
+		ext===undefined ? help() : '';
+		dataCollector.getCurrentMonthHistorySms(ext,cbHistSms);
+	break;
+	case 'hist_callnotes_day':
+		var ext = args[1];
+		var datex = args[2];
+		(ext===undefined || datex===undefined) ? help() : '';
+		dataCollector.getDayHistoryCallNotes(ext,datex,cbHistCallNotes);
+	break;
+	case 'hist_callnotes_curr_week':
+		var ext = args[1];
+		ext===undefined ? help() : '';
+		dataCollector.getCurrentWeekHistoryCallNotes(ext,cbHistCallNotes);
+	break;
+	case 'hist_callnotes_curr_month':
+		var ext = args[1];
+		ext===undefined ? help() : '';
+		dataCollector.getCurrentMonthHistoryCallNotes(ext,cbHistCallNotes);
 	break;
 	case 'ini':
 		var queries = dataCollector.getQueries();
@@ -46,6 +78,12 @@ function cbHistCall(result){
 function cbHistCallCurrMonth(result){
 	printArr(result,'Results:',false);
 	helpExit();
+}
+function cbHistSms(result){
+	printArr(result,'Results:',true);
+}
+function cbHistCallNotes(result){
+	printArr(result,'Results:',true);
 }
 function cbCC(result){
 	printArr(result,'Results:',true);
@@ -77,20 +115,44 @@ function help(){
 	cmd = cmd[cmd.length-1];
 	console.log(cmd);
 	console.log("NAME\n\t"+cmd+" - test DB query through dataCollector.js object. Configuration file is config/dataprofiles.ini\n");
-	console.log("SYNOPSIS\n\t" + allargs[0] + " " + cmd + " TYPE [OPTIONS]\n");
+	console.log("SYNOPSIS\n\t" + allargs[0] + " " + cmd + " TYPE [OPTIONS]");
+	console.log("\n");
 	console.log("OPTIONS");
 	console.log("\tini");
-	console.log("\t\tprint configuration file used by dataCollector\n");
+	console.log("\t\tprint configuration file used by dataCollector");
+	console.log("\n");
 	console.log("\tphonebook EXTENSION");
-	console.log("\t\tview result of phonebook query for user specified as EXTENSION\n");
+	console.log("\t\tview result of phonebook query for user specified as EXTENSION");
+	console.log("\n");
 	console.log("\tcc TYPE EXTENSION");
-	console.log("\t\tview customer card of type TYPE for user EXTENSION\n");
+	console.log("\t\tview customer card of type TYPE for user EXTENSION");
+	console.log("\n");
 	console.log("\thist_call_day EXTENSION DATE");
-	console.log("\t\tview history calls of the EXTENSION for specified DATE (yyyy/mm/gg)\n");
+	console.log("\t\tview history calls of the EXTENSION for specified DATE (yyyy/mm/gg)");
+	console.log("\n");
 	console.log("\thist_call_currweek EXTENSION");
-	console.log("\t\tview history calls of the EXTENSION for current week\n");
+	console.log("\t\tview history calls of the EXTENSION for current week");
+	console.log("\n");
 	console.log("\thist_call_currmonth EXTENSION");
-	console.log("\t\tview history calls of the EXTENSION for current month\n");
+	console.log("\t\tview history calls of the EXTENSION for current month");
+	console.log("\n");
+	console.log("\thist_sms_day EXTENSION DATE");
+	console.log("\t\tview history sms of the EXTENSION for specified DATE (yyyy/mm/gg)");
+	console.log("\n");
+	console.log("\thist_sms_currweek EXTENSION");
+	console.log("\t\tview history sms of the EXTENSION for current week");
+	console.log("\n");
+	console.log("\thist_sms_currmonth EXTENSION");
+	console.log("\t\tview history sms of the EXTENSION for current month");
+	console.log("\n");
+	console.log("\thist_callnotes_day EXTENSION DATE");
+	console.log("\t\tview history call notes of the EXTENSION for specified DATE (yyyy/mm/gg)");
+	console.log("\n");
+	console.log("\thist_callnotes_currweek EXTENSION");
+	console.log("\t\tview history call notes of the EXTENSION for current week");
+	console.log("\n");
+	console.log("\thist_callnotes_currmonth EXTENSION");
+	console.log("\t\tview history call notes of the EXTENSION for current month");
 	console.log("EXAMPLE\n\t" + allargs[0] + " " + cmd + " phonebook net");
 	console.log("\t" + allargs[0] + " " + cmd + " cc default net");
 	process.exit(0);
