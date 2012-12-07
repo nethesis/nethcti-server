42ddc31b98666aa11e74dac5b36b4ebded4749dc
Alessandro Polidori <alessandro.polidori@gmail.com>
2012-12-07 16:22:52 +0100

    fillCurrentCallInInfo also in phonebook when UserEvent

--- proxycti.js
+++ proxycti.js
@@ -2373,11 +2373,9 @@ function fillCurrentCallInInfo(num){
                                             break;
                                         }
                                     }
-                                    if (currentCallInInfo[num].ctiPublic !== undefined) {
-                                        dataCollector.getAllContactsByNum(num, numToSearch, function (results, num) {
-                                            currentCallInInfo[num].centralPhonebook = results[0];
-                                        });
-                                    }
+                                    dataCollector.getAllContactsByNum(num, numToSearch, function (results, num) {
+                                        currentCallInInfo[num].centralPhonebook = results[0];
+                                    });
                                 });
                         });
                 });
