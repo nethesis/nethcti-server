b2a0ad531d8792345a47ab27b207a4022f4df46b
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-02-12 12:37:11 +0100

    catch UncaughtException in proxycti.js

--- proxycti.js
+++ proxycti.js
@@ -5,8 +5,11 @@ var config = [
     { packagePath: "./plugins/com_authe_rest" },
     { packagePath: "./plugins/com_hist_rest" }
 ];
-
 var app = architect.resolveConfig(config, __dirname);
 architect.createApp(app, function () {
     console.log("app created");
+});
+process.on('uncaughtException', function (err) {
+    console.log('UncaughtException !!!');
+    console.log(err);
 });
