5955eef0c385c13e7b9530c66bb54d70d7c84f60
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-02-19 11:58:20 +0100

    add documentation to extension.js

--- plugins/ast_proxy/extension.js
+++ plugins/ast_proxy/extension.js
@@ -1,15 +1,59 @@
-// Abstraction of an extension
+/**
+* Abstraction of an extension.
+*
+* @class Extension
+* @constructor
+*/
 exports.Extension = function (extension, channelType) {
     try {
-        var exten    = extension;
+
+        /**
+        * The Extension number.
+        *
+        * @property {string}
+        * @required
+        * @private
+        */
+        var exten = extension;
+
+        /**
+        * The channel type.
+        *
+        * @property {string}
+        * @required
+        * @private
+        */
         var chanType = channelType;
 
-        function toString()    { return getChanType() + '/' + getExten(); }
+        /**
+        * Return the extension number.
+        *
+        * @method getExten
+        * @return {string} The extension number
+        */
+        function getExten() { return exten; }
 
+        /**
+        * Return the readable string of the extension.
+        *
+        * @method toString
+        * @return {string} The readable description of the extension
+        */
+        function toString() { return getChanType() + '/' + getExten(); }
+
+        /**
+        * Return the channel type.
+        *
+        * @method getChanType
+        * @return {string} The channel type
+        */
+        function getChanType() { return chanType; }
+
+        // public interface
         return {
-            exten:    exten,
-            chanType: chanType,
-            toString: toString
+            getExten: getExten,
+            toString: toString,
+            chanType: getChanType
         };
 
     } catch (err) {
