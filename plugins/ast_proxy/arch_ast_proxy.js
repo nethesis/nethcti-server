73cbcc120394ba2f9c36d9380aa8c6de4cf1db91
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-02-19 20:17:38 +0100

    insert docs into arch_ast_proxy

--- plugins/ast_proxy/arch_ast_proxy.js
+++ plugins/ast_proxy/arch_ast_proxy.js
@@ -1,5 +1,8 @@
-// It's the architect component. It configures the asterisk proxy and
-// starts it.
+/**
+* The architect component that starts the asterisk proxy.
+*
+* @class arch_ast_proxy
+*/
 var astProxy = require('./ast_proxy');
 
 module.exports = function (options, imports, register) {
@@ -17,7 +20,26 @@ module.exports = function (options, imports, register) {
     // public interface for other architect components
     register(null, {
         astProxy: {
+            /**
+            * It's the _on_ method provided by _ast\_proxy_ module.
+            *
+            * @method on
+            * @param {string} type The name of the event
+            * @param {function} cb The callback to execute in response to the event
+            * @return {object} A subscription handle capable of detaching that subscription
+            */
             on: astProxy.on,
+
+            /**
+            * It's the _get_ method provided by _ast\_proxy_ module.
+            *
+            * @method get
+            * @param {object} obj The object with the command name to execute and optional parameters
+            *   @param {string} obj.command The command name to execute. A plugin command file with the
+            *   same name must be present into the appropriate directory
+            *   @param [obj.parameters] 0..n The parameters that can be used into the command plugin
+            * @param {function} cb The callback
+            */
             get: astProxy.get
         }
     });
