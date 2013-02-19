6ba17cb33b63c737fc38cd81c875c6c87941f59a
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-02-19 14:47:31 +0100

    add doc comments into arch_ast_proxy.js

--- plugins/ast_proxy/arch_ast_proxy.js
+++ plugins/ast_proxy/arch_ast_proxy.js
@@ -1,9 +1,17 @@
+// It's the architect component. It configures the asterisk proxy and
+// starts it.
 var astProxy = require('./ast_proxy');
 
 module.exports = function (options, imports, register) {
+    
+    try {
+        astProxy.config('/etc/nethcti/asterisk.ini');
+        astProxy.start();
+    } catch (err) {
+        console.log(err.stack);
+    }
 
-    astProxy.start(options);
-
+    // public interface for other architect components
     register(null, {
         astProxy: {
             on: astProxy.on,
