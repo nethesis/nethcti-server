1f8411ceca6e28f7e9aade4ff89db3c21c037d0d
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-02-19 11:57:41 +0100

    add documentation to action.js

--- plugins/ast_proxy/action.js
+++ plugins/ast_proxy/action.js
@@ -1,20 +1,49 @@
-// Contains the common operations for all asterisk actions
+/**
+* Provides for common operations for all asterisk actions.
+*
+* @class action
+* @static
+*/
 
-var SEP = '_'; // separator character to construct ActionID
+/**
+* Separator character to construct ActionID key of the asterisk action.
+*
+* @property SEP
+* @type string
+* @default '_'
+* @final
+* @readOnly
+* @private
+*/
+var SEP = '_';
 
-// return the value for the ActionID key
-// An ActionID value is composed by action name + separator char + timestamp
+/**
+* Returns the value for the ActionID key of the asterisk action.
+*
+* @method getActionId
+* @static
+* @param  {string} id The name for the action
+* @return {string} Unique value for the ActionID key.
+* The value is: id + _ + timestamp
+*/
 function getActionId(id) {
     if (id) { return id + SEP + (new Date()).getTime(); }
     return (new Date()).getTime();
 }
 
-// return the action name from the ActionID value
+/**
+* Returns the action name from the ActionID key of the asterisk action.
+*
+* @method getActionName
+* @static
+* @param {sring} actionid The value of the ActionID key
+* @return {string} The name of the action or undefined
+*/
 function getActionName(actionid) {
     if (actionid && actionid.indexOf(SEP) !== -1) { return actionid.split(SEP)[0]; }
     return undefined;
 }
 
-// exports methods
+// public interface
+exports.getActionId   = getActionId;
 exports.getActionName = getActionName;
-exports.getActionId = getActionId;
