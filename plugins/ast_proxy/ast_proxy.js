f2111a71ba486b947742d60b6e7cb442b5cc436f
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-02-19 12:01:04 +0100

    add doc and add config method to ast_proxy.js

--- plugins/ast_proxy/ast_proxy.js
+++ plugins/ast_proxy/ast_proxy.js
@@ -1,42 +1,108 @@
-// It is the asterisk proxy
+/**
+* Asterisk proxy with standard interface to use.
+*
+* @class ast_proxy
+* @static
+*/
+var fs           = require('fs');
 var ast          = require('asterisk-ami');
 var action       = require('./action');
-var pluginsCmd   = require('jsplugs')().require('./plugins/ast_proxy/plugins_command_11');
 var iniparser    = require('iniparser');
+var pluginsCmd   = require('jsplugs')().require('./plugins/ast_proxy/plugins_command_11');
 var EventEmitter = require('events').EventEmitter;
 
-var am;       // asterisk manage
-var map = {}; // key: ActionID, value: cb
+/**
+* The asterisk manager.
+*
+* @property am
+* @type {object}
+* @private
+*/
+var am;
+
+/**
+* Event emitter
+*
+* @property emitter
+* @type object
+* @private
+*/
 var emitter = new EventEmitter();
 
-function start(options) {
+/**
+* The asterisk connection parameters
+*
+* @property astConf
+* @type object
+* @private
+*/
+var astConf = {};
+
+/**
+* Set configuration to use by telnet asterisk connection. **The method can throw an Exception.**
+*
+* @method config
+* @param {string|object} config The configuration to be used
+* for the telnet asterisk connection
+*   @param {string} config.path Path of the ini configuration file.
+*   The file must contain host, port, user and password key-value pairs
+*   into a single section called ASTERISK
+*   @param {object} config.object The object with host, port user and password keys
+*     @param {string} [config.object.host=localhost] The address of the asterisk server
+*     @param {string|number} [config.object.port=5038] The port of the asterisk server
+*     @param {string} config.object.user The username
+*     @param {string} config.object.pass The password
+*/
+function config(config) {
+    if (typeof config === 'string') {
+        if (!fs.existsSync(config)) { throw new Error(config + ' not exists'); }
+        var ini = iniparser.parseSync(config);
+        astConf = {
+            host:     ini.ASTERISK.host,
+            port:     ini.ASTERISK.port,
+            username: ini.ASTERISK.user,
+            password: ini.ASTERISK.pass
+        };
+
+    } else if (typeof config === 'object'
+               && config.user
+               && config.pass) {
+
+        if (!config.port) { console.log("port non ce"); config.port = '5038'; }
+        if (!config.host) { console.log("host non ce"); config.host = 'localhost'; }
+
+        astConf = {
+            host:     config.host,
+            port:     config.port,
+            username: config.user,
+            password: config.pass
+        };
+
+    } else {
+        throw new TypeError('wrong parameters');
+    }
+}
+
+/**
+* Start the telnet connection with the asterisk server and listen
+* to any events.
+*
+* @method start
+*/
+function start() {
     try {
 
-        // read configuration file
-        try {
-            // check arguments
-            if (!options.iniPath) {
-                options.iniPath = '/etc/nethcti/asterisk.ini';
-                console.log('set default ini path to ' + options.iniPath);
-            }
-            var ini = iniparser.parseSync(options.iniPath);
-        } catch (err) {
-            console.log(err.stack);
-            return;
-        }
-
-        // initialize asterisk manager
-        var config = {
-            host:       ini.ASTERISK.host,
-            port:       ini.ASTERISK.port,
-            username:   ini.ASTERISK.user,
-            password:   ini.ASTERISK.pass
-        };
-        am = new ast(config);
+        // initialize the asterisk manager
+        am = new ast(astConf);
         console.log('initialized asterisk manager');
 
         // add event listeners to asterisk manager
-        am.on('ami_data', onData);
+        am.on('ami_data',              onData);
+        am.on('ami_socket_end',        amiSocketEnd);
+        am.on('ami_socket_error',      amiSocketError);
+        am.on('ami_socket_close',      amiSocketClose);
+        am.on('ami_socket_timeout',    amiSocketTimeout);
+        am.on('ami_socket_unwritable', amiSocketUnwritable);
         console.log('added event listeners to asterisk manager');
 
         // connect to asterisk
@@ -53,12 +119,87 @@ function start(options) {
     }
 }
 
-// receive response or event from asterisk
+/**
+* Handler for the _ami\_socket\_end_ event emitted when the other end
+* of the socket sends a FIN packet.
+*
+* @method amiSocketEnd
+* @private
+*/
+function amiSocketEnd() {
+    console.log('asterisk socket disconnected');
+}
+
+/**
+* Handler for the _ami\_socket\_timeout_ event emitted when the socket times out
+* from inactivity. This is only to notify that the socket has been idle. The user
+* must manually close the connection.
+*
+* @method amiSocketTimeout
+* @private
+*/
+function amiSocketTimeout() {
+    console.log('asterisk socket timeout');
+}
+
+/**
+* Handler for the _ami\_socket\_close_ event emitted once the socket
+* is fully closed. The argument had_error is a boolean which says if
+* the socket was closed due to a transmission error
+*
+* @method amiSocketClose
+* @private
+* @param {boolean} had_error It says if the socket was closed due to a
+* transmission error
+*/
+function amiSocketClose(had_error) {
+    console.log('asterisk socket close');
+    console.log(typeof had_error);
+    console.log(had_error);
+}
+
+/**
+* Handler for the _ami\_socket\_unwritable_ event emitted when the socket
+* isn't writable.
+*
+* @method amiSocketUnwritable
+* @private
+*/
+function amiSocketUnwritable() {
+    console.log('asterisk socket unwritable');
+}
+
+/**
+* Handler for the _ami\_socket\_error_ event emitted when an error occurs,
+* e.g. when lost the connection. The 'close' event will be calledd directly
+* following this event.
+*
+* @method amiSocketError
+* @private
+* @param {object} err The error object
+*/
+function amiSocketError(err) {
+    try { console.log(err.stack); }
+    catch (err) { console.log(err.stack); }
+}
+
+/**
+* Handler for the received data from asterisk. It calls the _execute_ method of the
+* command plugin with the name that corresponds to the ActionID value contained in
+* the _data_ object. Plugins must reside in the appropriate directory.
+*
+* e.g. If the
+* data contains _astVersion\_12345_ ActionID key, the _astVersion.js_ plugin must be present.
+*
+* @method onData
+* @private
+* @param {object} data The data received from asterisk
+*/
 function onData(data) {
     try {
         // get ActionId and action name
         var actionid = data.actionid;
-        var cmd = action.getActionName(actionid); // may be undefined
+        var cmd = action.getActionName(actionid);
 
         // check command plugin presence
         if (pluginsCmd[cmd]) {
@@ -70,17 +211,35 @@ function onData(data) {
     }
 }
 
-// register new event listener
-function on(event, listener) {
-    try { emitter.on(event, listener); }
-    catch (err) { console.log(err.stack); }
-}
-
-// request data from asterisk
+/**
+* Executes specified command and return result in the callback. It calls the _execute_
+* method of the command plugin that has the same name of the specified command. The plugin
+* is contained into the file that resides in the appropriate directory and must have a file
+* name equal to specified command parameter.
+*
+* e.g. If the command name is _astVersion_, the
+* _astVersion.js_ plugin must be present.
+*
+*
+* @method get
+* @param {object} obj The object with the command name to execute and optional parameters
+*   @param {string} obj.command The command name to execute. A plugin command file with the
+*   same name must be present into the appropriate directory
+*   @param [obj.parameters] 0..n The parameters that can be used into the command plugin
+* @param {function} cb The callback
+* @example
+* get({ command: 'astVersion' }, function (res) {
+*     console.log(res);
+* });
+*/
 function get(obj, cb) {
     try {
-        if (pluginsCmd[obj.command]) {
+        if (pluginsCmd[obj.command]
+            && pluginsCmd[obj.command].execute
+            && typeof pluginsCmd[obj.command].execute === 'function') {
+
             pluginsCmd[obj.command].execute(am, obj, cb);
+
         } else {
             console.log('no plugin for command ' + obj.command);
         }
@@ -89,7 +248,21 @@ function get(obj, cb) {
     }
 }
 
-// exports methods
+/**
+* Subscribe a callback function to a custom event fired by this object.
+*
+* @method on
+* @param {string} type The name of the event
+* @param {function} cb The callback to execute in response to the event
+* @return {object} A subscription handle capable of detaching that subscription
+*/
+function on(type, cb) {
+    try { return emitter.on(type, cb); }
+    catch (err) { console.log(err.stack); }
+}
+
+// public interface
 exports.on = on;
+exports.config = config;
 exports.start = start;
 exports.get = get;
