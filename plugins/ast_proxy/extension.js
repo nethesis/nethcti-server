8d797b9f4a50bd38f3caa8f4ac190ce8a4d54db9
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-02-20 10:09:43 +0100

    remove try/cacth from extension and fix indent

--- plugins/ast_proxy/extension.js
+++ plugins/ast_proxy/extension.js
@@ -5,58 +5,52 @@
 * @constructor
 */
 exports.Extension = function (extension, channelType) {
-    try {
+    /**
+    * The Extension number.
+    *
+    * @property {string}
+    * @required
+    * @private
+    */
+    var exten = extension;
 
-        /**
-        * The Extension number.
-        *
-        * @property {string}
-        * @required
-        * @private
-        */
-        var exten = extension;
+    /**
+    * The channel type.
+    *
+    * @property {string}
+    * @required
+    * @private
+    */
+    var chanType = channelType;
 
-        /**
-        * The channel type.
-        *
-        * @property {string}
-        * @required
-        * @private
-        */
-        var chanType = channelType;
+    /**
+    * Return the extension number.
+    *
+    * @method getExten
+    * @return {string} The extension number
+    */
+    function getExten() { return exten; }
 
-        /**
-        * Return the extension number.
-        *
-        * @method getExten
-        * @return {string} The extension number
-        */
-        function getExten() { return exten; }
+    /**
+    * Return the readable string of the extension.
+    *
+    * @method toString
+    * @return {string} The readable description of the extension
+    */
+    function toString() { return getChanType() + '/' + getExten(); }
 
-        /**
-        * Return the readable string of the extension.
-        *
-        * @method toString
-        * @return {string} The readable description of the extension
-        */
-        function toString() { return getChanType() + '/' + getExten(); }
+    /**
+    * Return the channel type.
+    *
+    * @method getChanType
+    * @return {string} The channel type
+    */
+    function getChanType() { return chanType; }
 
-        /**
-        * Return the channel type.
-        *
-        * @method getChanType
-        * @return {string} The channel type
-        */
-        function getChanType() { return chanType; }
-
-        // public interface
-        return {
-            getExten: getExten,
-            toString: toString,
-            chanType: getChanType
-        };
-
-    } catch (err) {
-        console.log(err.stack);
-    }
+    // public interface
+    return {
+        getExten: getExten,
+        toString: toString,
+        chanType: getChanType
+    };
 }
