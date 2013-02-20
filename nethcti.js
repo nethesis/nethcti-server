13790569a78e7d28e5ca58b3b726a38c8ad21441
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-02-20 14:18:01 +0100

    nethcti.js: add some row spaced

--- nethcti.js
+++ nethcti.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
+// Main application that starts all architect modules
+
 var architect = require('architect');
 
+// architect components
 var config = [
     { packagePath: "./plugins/logger" },
     { packagePath: "./plugins/nethcti" },
@@ -8,10 +11,12 @@ var config = [
     { packagePath: "./plugins/com_authe_rest" },
     { packagePath: "./plugins/com_hist_rest" }
 ];
+
 var app = architect.resolveConfig(config, __dirname);
-architect.createApp(app, function () {
-    console.log("app created");
+architect.createApp(app, function (resp) {
+    console.log('nethcti architect app created');
 });
+
 process.on('uncaughtException', function (err) {
     console.log('UncaughtException !!!');
     console.log(err.stack);
