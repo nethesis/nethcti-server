f3375e44f0c89a42d2ab994789d7f77baf7ce7f8
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-02-22 18:50:10 +0100

    new ast command plugin 'cfuGet.js'

---
+++ plugins/ast_proxy/plugins_command_11/cfuGet.js
@@ -0,0 +1,148 @@
+/**
+* @submodule plugins_command_11
+*/
+var action = require('../action');
+
+/**
+* The module identifier used by the logger.
+*
+* @property IDLOG
+* @type string
+* @private
+* @final
+* @readOnly
+* @default [cfuGet]
+*/
+var IDLOG = '[cfuGet]';
+
+(function() {
+    try {
+        /**
+        * The logger. It must have at least three methods: _info, warn and error._
+        *
+        * @property logger
+        * @type object
+        * @private
+        * @default console
+        */
+        var logger = console;
+
+        /**
+        * Map associations between ActionID and callback to execute at the end
+        * of the command
+        *
+        * @property map
+        * @type {object}
+        * @private
+        */
+        var map = {};
+
+        /**
+        * Extension number to be verified for call forward status on busy.
+        *
+        * @property exten
+        * @type {string}
+        * @private
+        */
+        var exten;
+
+        /**
+        * Command plugin to get the call forward status on busy of an extension.
+        *
+        * @class cfuGet
+        * @static
+        */
+        var cfuGet = {
+
+            /**
+            * Execute asterisk action to get the asterisk version.
+            * 
+            * @method execute
+            * @param {object} am Asterisk manager to send the action
+            * @param {object} args The object contains optional parameters
+            * passed to _get_ method of the ast_proxy component 
+            * @static
+            */
+            execute: function (am, args, cb) {
+                try {
+                    exten = args.exten;
+                    // action to get asterisk version
+                    var act = { Action: 'DBGet', Family: 'CFU', Key: args.exten };
+
+                    // set the action identifier
+                    act.ActionID = action.getActionId('cfuGet');
+
+                    // add association ActionID-callback
+                    map[act.ActionID] = cb;
+
+                    // send action to asterisk
+                    am.send(act);
+
+                } catch (err) {
+                    logger.error(IDLOG, err.stack);
+                }
+            },
+
+            /**
+            * It's called from _ast_proxy_ component for each data received
+            * from asterisk and relative to this command
+            *
+            * @method data
+            * @param {object} data The asterisk data for the current command
+            * @static
+            */
+            data: function (data) {
+                try {
+                    // check callback and info presence and execute it
+                    if (map[data.actionid] && data.event === 'DBGetResponse'
+                        && data.family === 'CFU'
+                        && data.key === exten
+                        && data.val) {
+
+                        map[data.actionid]({ cfu: 'yes', cfuExten: data.val });
+                        delete map[data.actionid]; // remove association ActionID-callback
+
+                    } else if (map[data.actionid] && data.response === 'Error') {
+                        map[data.actionid]({ cfu: 'no' });
+                        delete map[data.actionid]; // remove association ActionID-callback
+                    }
+
+                } catch (err) {
+                    logger.error(IDLOG, err.stack);
+                }
+            },
+
+            /**
+            * Set the logger to be used.
+            *
+            * @method setLogger
+            * @param {object} log The logger object. It must have at least
+            * three methods: _info, warn and error_
+            * @static
+            */
+            setLogger: function (log) {
+                try {
+                    if (typeof log === 'object'
+                        && typeof log.info  === 'function'
+                        && typeof log.warn  === 'function'
+                        && typeof log.error === 'function') {
+
+                        logger = log;
+                    } else {
+                        throw new Error('wrong logger object');
+                    }
+                } catch (err) {
+                    logger.error(IDLOG, err.stack);
+                }
+            }
+        };
+
+        // public interface
+        exports.data      = cfuGet.data;
+        exports.execute   = cfuGet.execute;
+        exports.setLogger = cfuGet.setLogger;
+
+    } catch (err) {
+        logger.error(IDLOG, err.stack);
+    }
+})();
