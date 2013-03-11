0fe86316ab9e3771e45c52237c11330877a3e16f
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-03-11 15:30:51 +0100

    change structure of Extension.js

--- plugins/ast_proxy/extension.js
+++ plugins/ast_proxy/extension.js
@@ -4,17 +4,25 @@
 * **It can throw exceptions.**
 *
 * @class Extension
-* @param {string} extension The extension number
-* @param {string} channelType The channel type, SIP, IAX, ...
+* @param {object} obj The object with all informations about extension
+*   @param {string} obj.name The name of the extension
+*   @param {string} obj.ip The ip address of the device
+*   @param {string} obj.port The port of the device
+*   @param {string} obj.exten The extension number
+*   @param {string} obj.chantype The channel type, SIP, IAX, ...
+*   @param {string} obj.status The status
+*   @param {string} obj.sipuseragent The user agent of the extension
 * @constructor
 * @return {object} The extension object.
 */
-exports.Extension = function (extension, channelType) {
-    // check parameters
-    if (typeof extension !== 'string'
-        || typeof channelType !== 'string') {
+exports.Extension = function (obj) {
+    // check parameter
+    if (!obj || obj.sipuseragent === undefined
+        || obj.ip    === undefined || obj.port     === undefined
+        || obj.name  === undefined || obj.status   === undefined
+        || obj.exten === undefined || obj.chantype === undefined ) {
 
-        throw new Error('wrong parameters');
+        throw new Error('wrong parameter');
     }
 
     /**
@@ -25,7 +33,25 @@ exports.Extension = function (extension, channelType) {
     * @required
     * @private
     */
-    var exten = extension;
+    var exten = obj.exten;
+
+    /**
+    * The ip address of the device.
+    *
+    * @property ip
+    * @type {string}
+    * @private
+    */
+    var ip = obj.ip;
+
+    /**
+    * The port of the device.
+    *
+    * @property port
+    * @type {string}
+    * @private
+    */
+    var port = obj.port;
 
     /**
     * The channel type.
@@ -35,7 +61,37 @@ exports.Extension = function (extension, channelType) {
     * @required
     * @private
     */
-    var chanType = channelType;
+    var chanType = obj.chantype;
+
+    /**
+    * The sip user agent.
+    *
+    * @property sipuseragent
+    * @type {string}
+    * @required
+    * @private
+    */
+    var sipuseragent = obj.sipuseragent;
+
+    /**
+    * The Extension status enumeration.
+    *
+    * @property STATUS_ENUM
+    * @type {object}
+    * @private
+    * @final
+    * @default ONLINE | OFFLINE
+    */
+    var STATUS_ENUM = { ONLINE: 'online', OFFLINE: 'offline' };
+
+    /**
+    * The Extension status.
+    *
+    * @property status
+    * @type {string}
+    * @private
+    */
+    var status = obj.status;
 
     /**
     * Return the extension number.
@@ -60,6 +116,20 @@ exports.Extension = function (extension, channelType) {
     * @return {string} The channel type
     */
     function getChanType() { return chanType; }
+
+    /**
+    * Set the extension status to online.
+    *
+    * @method setOnline
+    */
+    function setOnline() { status = STATUS_ENUM.ONLINE; }
+
+    /**
+    * Set the extension status to online.
+    *
+    * @method setOffline
+    */
+    function setOffline() { status = STATUS_ENUM.OFFLINE; }
 
     // public interface
     return {
