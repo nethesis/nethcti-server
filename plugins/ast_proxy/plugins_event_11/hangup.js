ac6e66789c6aa38082f4d883893d334d9f2a3d50
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-03-20 15:34:06 +0100

    fix ast event plugin hangup

--- plugins/ast_proxy/plugins_event_11/hangup.js
+++ plugins/ast_proxy/plugins_event_11/hangup.js
@@ -1,8 +1,8 @@
 /**
-* ................................
+* Manage the asterisk events.
 * 
 * @module ast_proxy
-* @submodule plugins_command_11
+* @submodule plugins_event_11
 */
 
 /**
@@ -26,6 +26,65 @@ var IDLOG = '[hangup]';
 */
 var astProxy;
 
+/**
+* The hangup causes.
+*
+* @property CAUSE
+* @type {object}
+* @private
+*/
+var CAUSE = {
+    1:   'unallocated',
+    2:   'no_route_transit_net',
+    3:   'no_route_destination',
+    5:   'misdialled_trunk_prefix',
+    6:   'channel_unacceptable',
+    7:   'call_awarded_delivered',
+    8:   'pre_empted',
+    14:  'number_ported_not_here',
+    16:  'normal_clearing',
+    17:  'user_busy',
+    18:  'no_user_response',
+    19:  'no_answer',
+    20:  'subscriber_absent',
+    21:  'call_rejected',
+    22:  'number_changed',
+    23:  'redirected_to_new_destination',
+    26:  'answered_elsewhere',
+    27:  'destination_out_of_order',
+    28:  'invalid_number_format',
+    29:  'facility_rejected',
+    30:  'response_to_status_enquiry',
+    31:  'normal_unspecified',
+    34:  'normal_circuit_congestion',
+    38:  'network_out_of_order',
+    41:  'normal_temporary_failure',
+    42:  'switch_congestion',
+    43:  'access_info_discarded',
+    44:  'requested_chan_unavail',
+    50:  'facility_not_subscribed',
+    52:  'outgoing_call_barred',
+    54:  'incoming_call_barred',
+    57:  'bearercapability_notauth',
+    58:  'bearercapability_notavail',
+    65:  'bearercapability_notimpl',
+    66:  'chan_not_implemented',
+    69:  'facility_not_implemented',
+    81:  'invalid_call_reference',
+    88:  'incompatible_destination',
+    95:  'invalid_msg_unspecified',
+    96:  'mandatory_ie_missing',
+    97:  'message_type_nonexist',
+    98:  'wrong_message',
+    99:  'ie_nonexist',
+    100: 'invalid_ie_contents',
+    101: 'wrong_call_state',
+    102: 'recovery_on_timer_expire',
+    103: 'mandatory_ie_length_error',
+    111: 'protocol_error',
+    127: 'interworking'
+};
+
 (function() {
     try {
         /**
@@ -39,22 +98,34 @@ var astProxy;
         var logger = console;
 
         /**
-        * Command plugin to get the asterisk vers.....................
+        * The plugin that handles the hangup event.
         *
         * @class hangup
         * @static
         */
         var hangup = {
             /**
-            * It's called from _ast_proxy_ component for each data received
-            * from asterisk and relative to this command.
+            * It's called from _ast_proxy_ component for each hangup event
+            * received from the asterisk.
             *
             * @method data
-            * @param {object} data The asterisk data for the current command
+            * @param {object} data The asterisk event data
             * @static
             */
             data: function (data) {
                 try {
+                    if (data
+                        && data.cause
+                        && data.channel
+                        && data.calleridnum
+                        && data.connectedlinenum
+                        && data.event === 'Hangup') {
+
+                        astProxy.proxyLogic.hangupConversation({ ext: data.calleridnum, ch: data.channel, cause: CAUSE[data.cause] });
+
+                    } else {
+                        logger.warn(IDLOG, 'hangup event not recognized');
+                    }
 
                 } catch (err) {
                     logger.error(IDLOG, err.stack);
