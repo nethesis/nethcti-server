a6bccc95ce935b75519a554b277d77f5751b7afe
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-03-28 10:55:13 +0100

    add try/catch in nethcti.js core

--- nethcti.js
+++ nethcti.js
@@ -3,21 +3,27 @@
 
 var architect = require('architect');
 
-// architect components
-var config = [
-    { packagePath: "./plugins/logger" },
-    { packagePath: "./plugins/authe" },
-    { packagePath: "./plugins/com_authe_rest" },
-    { packagePath: "./plugins/ast_proxy" },
-    { packagePath: "./plugins/com_nethcti_ws" }
-];
+try{
+    // architect components
+    var config = [
+        { packagePath: "./plugins/logger" },
+        { packagePath: "./plugins/authe" },
+        { packagePath: "./plugins/com_authe_rest" },
+        { packagePath: "./plugins/ast_proxy" },
+        { packagePath: "./plugins/com_nethcti_ws" }
+    ];
 
-var app = architect.resolveConfig(config, __dirname);
-architect.createApp(app, function (resp) {
-    console.log('nethcti architect app created');
-});
+    var app = architect.resolveConfig(config, __dirname);
 
-process.on('uncaughtException', function (err) {
-    console.log('UncaughtException !!!');
+    architect.createApp(app, function (resp) {
+        console.log('nethcti architect app created');
+    });
+
+    process.on('uncaughtException', function (err) {
+        console.log('UncaughtException !!!');
+        console.log(err.stack);
+    });
+
+} catch (err) {
     console.log(err.stack);
-});
+}
