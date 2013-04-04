c7a67c0af9c9ea0b0a8c6fb25d34f51ac9c3fbde
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-04-04 16:24:20 +0200

    add first content for queue.js object

---
+++ plugins/ast_proxy/queue.js
@@ -0,0 +1,89 @@
+/**
+* Abstraction of a queue.
+*
+* **It can throw exception.**
+*
+* @class Queue
+* @param {string} queueNum The queue number
+* @constructor
+* @return {object} The queue object.
+*/
+exports.Queue = function (queueNum) {
+    // check the parameter
+    if (typeof queueNum !== 'string') { throw new Error('wrong parameter'); }
+
+    /**
+    * The queue number.
+    *
+    * @property queue
+    * @type {string}
+    * @required
+    * @private
+    */
+    var queue = queueNum;
+
+    /**
+    * The queue name.
+    *
+    * @property name
+    * @type {string}
+    * @required
+    * @private
+    */
+    var name = '';
+
+    /**
+    * Set the queue name.
+    *
+    * @method setName
+    * @param {string} qName The queue name
+    */
+    function setName(qName) { name = qName; }
+
+    /**
+    * Return the queue name.
+    *
+    * @method getName
+    * @return {string} The queue name.
+    */
+    function getName() { return name; }
+
+    /**
+    * Return the queue number.
+    *
+    * @method getQueue
+    * @return {string} The queue number
+    */
+    function getQueue() { return queue; }
+
+    /**
+    * Return the readable string description of the queue.
+    *
+    * @method toString
+    * @return {string} The readable description of the extension
+    */
+    function toString() { return 'QUEUE/' + getQueue(); }
+
+    /**
+    * Returns an object literal representation of the object
+    * without any methods.
+    *
+    * @method marshallObjLiteral
+    * @return {object} The object literal representation of the object.
+    */
+    function marshallObjLiteral() {
+        return {
+            name:  name,
+            queue: queue
+        }
+    }
+
+    // public interface
+    return {
+        setName:   setName,
+        getName:   getName,
+        getQueue:  getQueue,
+        toString:  toString,
+        marshallObjLiteral: marshallObjLiteral
+    };
+}
