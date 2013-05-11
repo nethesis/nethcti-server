97710a7c7d2a10cb1024c0021df8fde4ee19f1aa
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-05-11 06:59:16 +0200

    added phonebook plugin module

---
+++ plugins/phonebook/arch_phonebook.js
@@ -0,0 +1,43 @@
+/**
+* The architect component that exposes _phonebook_ module.
+*
+* @class arch_phonebook
+* @module phonebook
+*/
+var phonebook = require('./phonebook');
+
+/**
+* The module identifier used by the logger.
+*
+* @property IDLOG
+* @type string
+* @private
+* @final
+* @readOnly
+* @default [arch_phonebook]
+*/
+var IDLOG = '[arch_phonebook]';
+
+module.exports = function (options, imports, register) {
+    
+    var logger = console;
+    if (imports.logger) { logger = imports.logger; }
+
+    // public interface for other architect components
+    register(null, {
+        phonebook: {
+            /**
+            * It's the _........_ method provided by _phonebook_ module.
+            *
+            * @method .........
+            */
+            //........: phonebook.........
+        }
+    });
+
+    try {
+        phonebook.setLogger(logger);
+    } catch (err) {
+        logger.error(IDLOG, err.stack);
+    }
+}
