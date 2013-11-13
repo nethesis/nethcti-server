bcca69fd0f8a6bec8c0b6c8abfc07bc3a6c8a935
Alessandro Polidori <alessandro.polidori@gmail.com>
2013-11-13 09:43:44 +0100

    fix create postit table in the docs example

--- docs/db_tables/postit.sql
+++ docs/db_tables/postit.sql
@@ -2,11 +2,11 @@ CREATE TABLE `postit` (
   `id` int(11) NOT NULL auto_increment,
   `text` varchar(255) default NULL,
   `creator` varchar(50) default NULL,
-  `dateread` timestamp NULL default NULL,
+  `readdate` datetime default NULL,
   `recipient` varchar(50) default NULL,
-  `datecreation` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `creation` datetime NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `index_creator` (`creator`),
   KEY `index_recipient` (`recipient`),
-  KEY `index_dateread` (`dateread`)
-) ENGINE=MyISAM AUTO_INCREMENT=49 DEFAULT CHARSET=utf8;
+  KEY `index_readdate` (`readdate`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
