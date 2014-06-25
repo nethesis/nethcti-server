27fa51793e85444422620427457990342e45792a
Alessandro Polidori <alessandro.polidori@gmail.com>
2014-06-25 14:34:00 +0100

    change script to get branch 2.1.6

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
-git archive --format=tar --remote=ssh://git.nethesis.it/var/git/nethcti-server.git master_nethserver_2.1 | gzip > nethcti-server-source.tar.gz
+git archive --format=tar --remote=ssh://git.nethesis.it/var/git/nethcti-server.git master_nethserver_2.1.6 | gzip > nethcti-server-source.tar.gz
 tar -zcf nethcti-server.tar.gz nethcti-server
