f0dcbf805294172d031ebd3b4011613a9057f363
Alessandro Polidori <alessandro.polidori@gmail.com>
2015-01-09 16:17:06 +0100

    main nethcti.js: auto load all plugins

--- nethcti.js
+++ nethcti.js
@@ -1,59 +1,59 @@
 #!/usr/bin/env node
 // Main application that starts all architect modules
 
-var architect = require('architect');
+var fs              = require('fs');
+var path            = require('path');
+var architect       = require('architect');
+var PLUGINS_DIRNAME = './plugins';
 
-try{
-    // architect components
-    var config = [
-        { packagePath: "./plugins/logger"                  },
-        { packagePath: "./plugins/util"                    },
-        { packagePath: "./plugins/user"                    },
-        { packagePath: "./plugins/authorization"           },
-        { packagePath: "./plugins/authentication"          },
-        { packagePath: "./plugins/dbconn"                  },
-        { packagePath: "./plugins/mailer"                  },
-        { packagePath: "./plugins/sms"                     },
-        { packagePath: "./plugins/cel"                     },
-        { packagePath: "./plugins/ast_proxy"               },
-        { packagePath: "./plugins/postit"                  },
-        { packagePath: "./plugins/history"                 },
-        { packagePath: "./plugins/phonebook"               },
-        { packagePath: "./plugins/operator"                },
-        { packagePath: "./plugins/streaming"               },
-        { packagePath: "./plugins/voicemail"               },
-        { packagePath: "./plugins/profiling"               },
-        { packagePath: "./plugins/caller_note"             },
-        { packagePath: "./plugins/config_manager"          },
-        { packagePath: "./plugins/customer_card"           },
-        { packagePath: "./plugins/exec_script"             },
-        { packagePath: "./plugins/notification_manager"    },
-        { packagePath: "./plugins/com_static_http"         },
-        { packagePath: "./plugins/com_authentication_rest" },
-        { packagePath: "./plugins/com_sms_rest"            },
-        { packagePath: "./plugins/com_postit_rest"         },
-        { packagePath: "./plugins/com_history_rest"        },
-        { packagePath: "./plugins/com_phonebook_rest"      },
-        { packagePath: "./plugins/com_streaming_rest"      },
-        { packagePath: "./plugins/com_ast_proxy_rest"      },
-        { packagePath: "./plugins/com_voicemail_rest"      },
-        { packagePath: "./plugins/com_caller_note_rest"    },
-        { packagePath: "./plugins/com_authorization_rest"  },
-        { packagePath: "./plugins/com_customer_card_rest"  },
-        { packagePath: "./plugins/com_config_manager_rest" },
-        { packagePath: "./plugins/com_profiling_rest"      },
-        { packagePath: "./plugins/com_nethcti_tcp"         },
-        { packagePath: "./plugins/com_nethcti_ws"          },
-        { packagePath: "./plugins/http_proxy"              }
-    ];
+try {
+    // create list of modules to be loaded reading the plugins directory content
+    var modules = [];
+    fs.readdir(PLUGINS_DIRNAME, function (err, files) {
+        try {
+            if (err) {
+                console.log(err.stack);
+                return;
+            }
 
-    var app = architect.resolveConfig(config, __dirname);
+            var i;
+            for (i = 0; i < files.length; i++) {
+                modules.push(PLUGINS_DIRNAME + path.sep + files[i]);
+            }
 
-    architect.createApp(app, function (resp) {});
+            // load config
+            architect.resolveConfig(modules, __dirname, function (err, config) {
+                try {
+                    if (err) {
+                        console.log(err.stack);
+                        return;
+                    }
 
-    process.on('uncaughtException', function (err) {
-        console.log('UncaughtException !!!');
-        console.log(err.stack);
+                    // create the app
+                    architect.createApp(config, function (err, app) {
+                        try {
+                            if (err) {
+                                console.log(err.stack);
+                                return;
+                            }
+                        } catch (err) {
+                            console.log(err.stack);
+                        }
+                    });
+
+                } catch (err) {
+                    console.log(err.stack);
+                }
+            });
+
+            process.on('uncaughtException', function (err) {
+                console.log('UncaughtException !!!');
+                console.log(err.stack);
+            });
+
+        } catch (err) {
+            console.log(err.stack);
+        }
     });
 
 } catch (err) {
