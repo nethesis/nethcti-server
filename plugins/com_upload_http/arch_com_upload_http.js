0aadc033137116a3c4c385c4893b7cc8b8228a65
Alessandro Polidori <alessandro.polidori@gmail.com>
2015-03-12 01:22:45 +0100

    new service for upload file. Refs #3498

---
+++ plugins/com_upload_http/arch_com_upload_http.js
@@ -0,0 +1,36 @@
+/**
+* The architect component that starts _server\_com\_upload\_http_ module.
+*
+* @class arch_com_upload_http
+* @module com_upload_http
+*/
+var serverRest = require('./server_com_upload_http.js');
+
+/**
+* The module identifier used by the logger.
+*
+* @property IDLOG
+* @type string
+* @private
+* @final
+* @readOnly
+* @default [arch_com_upload_http]
+*/
+var IDLOG = '[arch_com_upload_http]';
+
+module.exports = function (options, imports, register) {
+
+    register(null, { uploadHttp: {} });
+
+    var logger = console;
+    if (imports.logger) { logger = imports.logger; }
+
+    try {
+        serverRest.setLogger(logger);
+        serverRest.config('/etc/nethcti/services.json');
+        serverRest.setCompUtil(imports.util);
+        serverRest.start();
+    } catch (err) {
+       logger.error(IDLOG, err.stack);
+    }
+}
