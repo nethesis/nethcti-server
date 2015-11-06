62306a269f15a21fa7c03bdd8c056152d6a9dac7
Stefano Fancello <stefano.fancello@nethesis.it>
2015-11-06 16:57:15 +0100

    script.sh accept branch to clone as an argument

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -x
-git archive --format=tar --remote=ssh://git.nethesis.it/var/git/nethcti-server.git master | gzip > nethcti-server-source.tar.gz
+if [[ -z $1 ]] ; then 
+    BRANCH="master";
+else 
+    BRANCH=$1
+fi
+git archive --format=tar --remote=ssh://git.nethesis.it/var/git/nethcti-server.git $BRANCH | gzip > nethcti-server-source.tar.gz
 tar -zcf nethcti-server.tar.gz nethcti-server
