07a58108eca68eb437a27d60683f60b3a6a68b09
Alessandro Polidori <alessandro.polidori@nethesis.it>
2017-02-09 12:49:21 +0100

    add plugin websocket

---
+++ root/usr/lib/node/nethcti-server/plugins/com_nethcti_ws/arch_com_nethcti_ws.js
@@ -0,0 +1,50 @@
+/**
+ * Supply the real time information.
+ *
+ * @class arch_com_nethcti_ws
+ * @module com_nethcti_ws
+ */
+var comNethctiWs = require('./com_nethcti_ws');
+
+/**
+ * The module identifier used by the logger.
+ *
+ * @property IDLOG
+ * @type string
+ * @private
+ * @final
+ * @readOnly
+ * @default [arch_com_nethcti_ws]
+ */
+var IDLOG = '[arch_com_nethcti_ws]';
+
+module.exports = function(options, imports, register) {
+
+  register(null, {
+    com_nethcti_ws: comNethctiWs
+  });
+
+  try {
+    var logger = console;
+
+    if (imports.logger) {
+      logger = imports.logger;
+    }
+
+    // wait for the authentication component ready event
+    imports.authentication.on(imports.authentication.EVT_COMP_READY, function() {
+      comNethctiWs.setLogger(logger);
+      comNethctiWs.setAuthe(imports.authentication);
+      comNethctiWs.config('/etc/nethcti/services.json');
+      comNethctiWs.configPrivacy('/etc/nethcti/nethcti.json');
+      comNethctiWs.setCompUser(imports.user);
+      // comNethctiWs.setCompPostit(imports.postit);
+      // comNethctiWs.setAstProxy(imports.astProxy);
+      // comNethctiWs.setCompVoicemail(imports.voicemail);
+      // comNethctiWs.setCompAuthorization(imports.authorization);
+      comNethctiWs.start();
+    });
+  } catch (err) {
+    logger.error(IDLOG, err.stack);
+  }
+}
