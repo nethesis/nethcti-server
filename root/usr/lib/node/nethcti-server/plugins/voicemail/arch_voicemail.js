3aac95d0bcfa9146607688538730ecc1f89dea0a
Alessandro Polidori <alessandro.polidori@nethesis.it>
2017-04-12 14:07:45 +0200

    jslint & fix indentation of voicemail

--- root/usr/lib/node/nethcti-server/plugins/voicemail/arch_voicemail.js
+++ root/usr/lib/node/nethcti-server/plugins/voicemail/arch_voicemail.js
@@ -1,103 +1,49 @@
 /**
-* The architect component that exposes _voicemail_ module.
-*
-* @class arch_voicemail
-* @module voicemail
-*/
+ * The architect component that exposes _voicemail_ module.
+ *
+ * @class arch_voicemail
+ * @module voicemail
+ */
 var voicemail = require('./voicemail');
 
 /**
-* The module identifier used by the logger.
-*
-* @property IDLOG
-* @type string
-* @private
-* @final
-* @readOnly
-* @default [arch_voicemail]
-*/
+ * The module identifier used by the logger.
+ *
+ * @property IDLOG
+ * @type string
+ * @private
+ * @final
+ * @readOnly
+ * @default [arch_voicemail]
+ */
 var IDLOG = '[arch_voicemail]';
 
-module.exports = function (options, imports, register) {
+module.exports = function(options, imports, register) {
 
-    var logger = console;
-    if (imports.logger) { logger = imports.logger; }
+  var logger = console;
+  if (imports.logger) {
+    logger = imports.logger;
+  }
 
-    // public interface for other architect components
-    register(null, {
-        voicemail: {
-            /**
-            * It's the _on_ method provided by _voicemail_ module.
-            *
-            * @method on
-            */
-            on: voicemail.on,
+  // public interface for other architect components
+  register(null, {
+    voicemail: voicemail
+  });
 
-            /**
-            * It's the _getVoiceMessagesByUser_ method provided by _voicemail_ module.
-            *
-            * @method getVoiceMessagesByUser
-            */
-            getVoiceMessagesByUser: voicemail.getVoiceMessagesByUser,
+  try {
+    voicemail.setLogger(logger);
 
-            /**
-            * It's the _deleteVoiceMessage_ method provided by _voicemail_ module.
-            *
-            * @method deleteVoiceMessage
-            */
-            deleteVoiceMessage: voicemail.deleteVoiceMessage,
-
-            /**
-            * It's the _listenVoiceMessage_ method provided by _voicemail_ module.
-            *
-            * @method listenVoiceMessage
-            */
-            listenVoiceMessage: voicemail.listenVoiceMessage,
-
-            /**
-            * It's the _getAllNewVoiceMessageCount_ method provided by _voicemail_ module.
-            *
-            * @method getAllNewVoiceMessageCount
-            */
-            getAllNewVoiceMessageCount: voicemail.getAllNewVoiceMessageCount,
-
-            /**
-            * It's the _getVmIdFromDbId_ method provided by _voicemail_ module.
-            *
-            * @method getVmIdFromDbId
-            */
-            getVmIdFromDbId: voicemail.getVmIdFromDbId,
-
-            /**
-            * It's the _EVT\_NEW\_VOICE\_MESSAGE_ property provided by _voicemail_ module.
-            *
-            * @method EVT_NEW_VOICE_MESSAGE
-            */
-            EVT_NEW_VOICE_MESSAGE: voicemail.EVT_NEW_VOICE_MESSAGE,
-
-            /**
-            * It's the _EVT\_UPDATE\_NEW\_VOICE\_MESSAGES_ property provided by _voicemail_ module.
-            *
-            * @method EVT_UPDATE_NEW_VOICE_MESSAGES
-            */
-            EVT_UPDATE_NEW_VOICE_MESSAGES: voicemail.EVT_UPDATE_NEW_VOICE_MESSAGES
-        }
+    // wait for the creation of the users
+    imports.user.on(imports.user.EVT_USERS_READY, function() {
+      voicemail.setCompUser(imports.user);
+      voicemail.setAstProxy(imports.astProxy);
+      voicemail.start();
     });
 
-    try {
-        voicemail.setLogger(logger);
-
-        // wait for the creation of the users
-        imports.user.on(imports.user.EVT_USERS_READY, function () {
-            voicemail.setCompUser(imports.user);
-            voicemail.setAstProxy(imports.astProxy);
-            voicemail.start();
-        });
-
-        imports.dbconn.on(imports.dbconn.EVT_READY, function () {
-            voicemail.setDbconn(imports.dbconn);
-        });
-    } catch (err) {
-        logger.error(IDLOG, err.stack);
-    }
-}
+    imports.dbconn.on(imports.dbconn.EVT_READY, function() {
+      voicemail.setDbconn(imports.dbconn);
+    });
+  } catch (err) {
+    logger.error(IDLOG, err.stack);
+  }
+};
