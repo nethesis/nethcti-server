4af7d1b80d2fcd70b8f2234b2c9d6db22745c719
Alessandro Polidori <alessandro.polidori@nethesis.it>
2018-05-24 14:24:14 +0200

    Send conf events to all involved clients
    nethesis/dev#5408

--- root/usr/lib/node/nethcti-server/plugins/com_nethcti_ws/com_nethcti_ws.js
+++ root/usr/lib/node/nethcti-server/plugins/com_nethcti_ws/com_nethcti_ws.js
@@ -1321,7 +1321,10 @@ function meetmeConfChanged(conf) {
   try {
     logger.log.info(IDLOG, 'received event "' + astProxy.EVT_MEETME_CONF_CHANGED + '" for conf id ' + conf.getId());
     logger.log.info(IDLOG, 'emit event "' + EVT_MEETME_CONF_UPDATE + '" for conf id ' + conf.getId() + ' to websockets');
-    sendEvtToUserWithExtenId(EVT_MEETME_CONF_UPDATE, conf.toJSON(), conf.getId());
+    var extens = Object.keys(conf.getAllUsers());
+    for (var i = 0; i < extens.length; i++) {
+      sendEvtToUserWithExtenId(EVT_MEETME_CONF_UPDATE, conf.toJSON(), extens[i]);
+    }
   } catch (err) {
     logger.log.error(IDLOG, err.stack);
   }
@@ -1340,9 +1343,10 @@ function meetmeConfEnd(confId) {
   try {
     logger.log.info(IDLOG, 'received event "' + astProxy.EVT_MEETME_CONF_END + '" for conf id ' + confId);
     logger.log.info(IDLOG, 'emit event "' + EVT_MEETME_CONF_END + '" for conf id ' + confId + ' to websockets');
-    sendEvtToUserWithExtenId(EVT_MEETME_CONF_END, {
-      id: confId
-    }, confId);
+    var extens = Object.keys(conf.getAllUsers());
+    for (var i = 0; i < extens.length; i++) {
+      sendEvtToUserWithExtenId(EVT_MEETME_CONF_END, { id: confId }, extens[i]);
+    }
   } catch (err) {
     logger.log.error(IDLOG, err.stack);
   }
