ea814d422bfb4476bece42988d8274a362b7c868
Alessandro Polidori <alessandro.polidori@nethesis.it>
2019-05-28 15:37:35 +0200

    Fix test/collectd-alarm.sh to use a specific queue
    nethesis/dev#5635

--- root/usr/lib/node/nethcti-server/test/collectd-alarm.sh
+++ root/usr/lib/node/nethcti-server/test/collectd-alarm.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-/usr/bin/echo -e "PUTNOTIF host=$(hostname) type=queuefewop type_instance=252 severity=warning time=$(date +%s) message=\"foo bar\"" | /usr/bin/nc -U /var/run/collectd.sock
-echo "alarm \"queuefewop\" alarm emitted with severity \"warning\""
+if [ $# -eq 0 ]; then
+  echo "use: $0 <QUEUE_NUMBER>"
+  exit 1
+fi
+QUEUE=$1
+/usr/bin/echo -e "PUTNOTIF host=$(hostname) type=queuefewop type_instance=$QUEUE severity=warning time=$(date +%s) message=\"foo bar\"" | /usr/bin/nc -U /var/run/collectd.sock
+echo "alarm \"queuefewop\" alarm emitted with severity \"warning\" for QUEUE $QUEUE"
