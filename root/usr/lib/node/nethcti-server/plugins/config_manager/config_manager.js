57263c765a33d405d91634748ce14d8c554c05f9
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2020-11-06 09:35:18 +0100

    nethcti-server: add SIP proxy port

--- root/usr/lib/node/nethcti-server/plugins/config_manager/config_manager.js
+++ root/usr/lib/node/nethcti-server/plugins/config_manager/config_manager.js
@@ -147,6 +147,16 @@ var compComNethctiWs;
 var serverHostname = '';
 
 /**
+ * The SIP proxy port number. It will be customized by the _config_ method.
+ *
+ * @property proxyPort
+ * @type integer
+ * @private
+ * @default ""
+ */
+var proxyPort;
+
+/**
  * The server chat parameters. It can be customized using the JSON
  * configuration file by means _configChat_ method.
  *
@@ -536,7 +546,7 @@ function configChat(path) {
 }
 
 /**
- * It reads the JSON configuration file and set the server ip address.
+ * It reads the JSON configuration file and set the server ip address and proxy port.
  *
  * @method config
  * @param {string} path The path of the configuration file
@@ -564,6 +574,12 @@ function config(path) {
       return;
     }
     serverHostname = json.hostname;
+
+    // set proxy port
+    if (typeof json.proxy_port == "number") {
+        proxyPort = json.proxy_port;
+    }
+
     logger.log.info(IDLOG, 'configuration done by ' + CONFIG_FILEPATH);
 
   } catch (err) {
@@ -879,6 +895,21 @@ function getServerHostname() {
     logger.log.error(IDLOG, err.stack);
   }
 }
+
+/**
+ * Returns the SIP proxy port.
+ *
+ * @method getProxyPort
+ * @return {integer} The SIP proxy port.
+ */
+function getProxyPort() {
+  try {
+    return proxyPort;
+  } catch (err) {
+    logger.log.error(IDLOG, err.stack);
+  }
+}
+
 
 /**
  * It reads the JSON configuration file of phone urls used to
@@ -1975,6 +2006,7 @@ exports.setCompAstProxy = setCompAstProxy;
 exports.setCompAuthorization = setCompAuthorization;
 exports.configPhoneUrls = configPhoneUrls;
 exports.getServerHostname = getServerHostname;
+exports.getProxyPort = getProxyPort
 exports.getDtmfUrlFromAgent = getDtmfUrlFromAgent;
 exports.setCompComNethctiWs = setCompComNethctiWs;
 exports.getCallUrlFromAgent = getCallUrlFromAgent;
