827dba63e846c3cf24b2573d615bc2e6416e3ab7
Alessandro Polidori <alessandro.polidori@nethesis.it>
2021-01-14 15:41:43 +0100

    Add support to enable Nethifier log (#204)
    nethesis/dev#5948

--- root/usr/lib/node/nethcti-server/nethcti-cli.js
+++ root/usr/lib/node/nethcti-server/nethcti-cli.js
@@ -4,11 +4,15 @@ const name = 'nethcti.sock';
 const CMD = process.argv[2];
 const ALLOWED_CMDS = {
   reload: 'reload',
-  dump: 'dump'
+  dump: 'dump',
+  nethifier_log: 'nethifier_log'
 };
 
-if (ALLOWED_CMDS[CMD] === undefined) {
-  console.log(`usage: node nethcti-cli (${Object.keys(ALLOWED_CMDS).join('|')})`);
+if (ALLOWED_CMDS[CMD] === undefined ||
+  (CMD === ALLOWED_CMDS.nethifier_log && process.argv.length !== 5)) {
+  let cmdlist = Object.keys(ALLOWED_CMDS).join('|');
+  cmdlist = cmdlist.replace('nethifier_log', 'nethifier_log <USERNAME> ("on" | "off")');
+  console.log(`usage: node nethcti-cli (${cmdlist})`);
   process.exit(2);
 }
 
@@ -22,7 +26,20 @@ ipc.config.rawBuffer = true;
 
 ipc.connectTo(name, () => {
   ipc.of[name].on('connect', () => {
-    ipc.of[name].emit(JSON.stringify({ type: 'message', data: CMD }));
+
+    if (CMD === ALLOWED_CMDS.nethifier_log) {
+      ipc.of[name].emit(JSON.stringify({
+        type: 'message',
+        data: CMD,
+        username: process.argv[3],
+        state: process.argv[4]
+      }));
+    } else {
+      ipc.of[name].emit(JSON.stringify({
+        type: 'message',
+        data: CMD
+      }));
+    }
     if (CMD === ALLOWED_CMDS.reload) {
       ipc.disconnect(name);
     }
@@ -39,6 +56,8 @@ ipc.connectTo(name, () => {
       data = JSON.parse(data.toString());
       if (data.type === 'dump') {
         console.log(JSON.stringify(data.data, null, 2));
+      } else if (data.type === 'nethifier_log') {
+        console.log(JSON.stringify(data, null, 2));
       }
       ipc.disconnect(name);
     } catch (err) {
