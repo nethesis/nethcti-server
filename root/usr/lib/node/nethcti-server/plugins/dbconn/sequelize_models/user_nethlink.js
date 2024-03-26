6c5a8803f239af2b1f258d85e87e91031e72bc60
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2024-03-26 16:17:35 +0100

    dbconn. sequelize_models: removed user_nethlink

--- root/usr/lib/node/nethcti-server/plugins/dbconn/sequelize_models/user_nethlink.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('user_nethlink', {
-      user: DataTypes.STRING,
-      extension: DataTypes.STRING,
-      timestamp: DataTypes.DATE,
-    });
-  };
