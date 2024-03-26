058de06e78c8aa1897116c6b709e1c588c97354f
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2024-03-26 14:58:06 +0100

    sequelize_models. user_nethlink: removed id attribute

--- root/usr/lib/node/nethcti-server/plugins/dbconn/sequelize_models/user_nethlink.js
+++ root/usr/lib/node/nethcti-server/plugins/dbconn/sequelize_models/user_nethlink.js
@@ -1,10 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
     return sequelize.define('user_nethlink', {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
       user: DataTypes.STRING,
       extension: DataTypes.STRING,
       timestamp: DataTypes.DATE,
