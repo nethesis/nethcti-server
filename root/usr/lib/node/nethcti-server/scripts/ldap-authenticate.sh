bd78cf61226e6bff087cfd7b1f7221505ede4dca
Stefano Fancello <gentoo.stefano@gmail.com>
2024-07-09 17:49:30 +0200

    fix indentation

--- root/usr/lib/node/nethcti-server/scripts/ldap-authenticate.sh
+++ root/usr/lib/node/nethcti-server/scripts/ldap-authenticate.sh
@@ -14,12 +14,12 @@ if [ "$NETHVOICE_LDAP_SCHEMA" = "rfc2307" ]; then
         -D "uid=$1,ou=People,$NETHVOICE_LDAP_BASE" \
         -w "$2" >  /dev/null
 elif [ "$NETHVOICE_LDAP_SCHEMA" = "ad" ]; then
-	if echo "$NETHVOICE_LDAP_USER" | grep -q '@' ; then
+    if echo "$NETHVOICE_LDAP_USER" | grep -q '@' ; then
         NETHVOICE_AD_DOMAIN=$(echo "$NETHVOICE_LDAP_USER" | sed 's/.*@\(.*\)/\1/')
-	else
-	    NETHVOICE_AD_DOMAIN=$(echo "$NETHVOICE_LDAP_USER" | tr '[:upper:]' '[:lower:]' | rev | sed 's/\(.*\)=cd.*/\1/;s/=cd,/./g' | rev)
+    else
+        NETHVOICE_AD_DOMAIN=$(echo "$NETHVOICE_LDAP_USER" | tr '[:upper:]' '[:lower:]' | rev | sed 's/\(.*\)=cd.*/\1/;s/=cd,/./g' | rev)
     fi
-	exec ldapsearch \
+    exec ldapsearch \
         -x \
         -s base \
         -b "$NETHVOICE_LDAP_BASE" \
