4edc609524894fad6be99e45cd9056503c4e90e4
Stefano <gentoo.stefano@gmail.com>
2024-07-10 08:42:09 +0200

    Guess the AD domain if it is expressed as LDAP binddn (#315)
    * Guess the AD domain if it is expressed as LDAP binddn
    
    If the LDAP base is something like "CN=foo,OU=bar,DC=example,DC=com" is converted in "example.com"

--- root/usr/lib/node/nethcti-server/scripts/ldap-authenticate.sh
+++ root/usr/lib/node/nethcti-server/scripts/ldap-authenticate.sh
@@ -5,7 +5,7 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-if [ $NETHVOICE_LDAP_SCHEMA = "rfc2307" ]; then
+if [ "$NETHVOICE_LDAP_SCHEMA" = "rfc2307" ]; then
     exec ldapsearch \
         -x \
         -s base \
@@ -13,8 +13,12 @@ if [ $NETHVOICE_LDAP_SCHEMA = "rfc2307" ]; then
         -H "ldap://$NETHVOICE_LDAP_HOST:$NETHVOICE_LDAP_PORT" \
         -D "uid=$1,ou=People,$NETHVOICE_LDAP_BASE" \
         -w "$2" >  /dev/null
-elif [ $NETHVOICE_LDAP_SCHEMA = "ad" ]; then
-    NETHVOICE_AD_DOMAIN=$(echo $NETHVOICE_LDAP_USER | sed 's/.*@\(.*\)/\1/')
+elif [ "$NETHVOICE_LDAP_SCHEMA" = "ad" ]; then
+    if echo "$NETHVOICE_LDAP_USER" | grep -q '@' ; then
+        NETHVOICE_AD_DOMAIN=$(echo "$NETHVOICE_LDAP_USER" | sed 's/.*@\(.*\)/\1/')
+    else
+        NETHVOICE_AD_DOMAIN=$(echo "$NETHVOICE_LDAP_USER" | tr '[:upper:]' '[:lower:]' | rev | sed 's/\(.*\)=cd.*/\1/;s/=cd,/./g' | rev)
+    fi
     exec ldapsearch \
         -x \
         -s base \
