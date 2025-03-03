1000734d829af454fdb5c9dcee953f07ed8dd48a
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2025-03-03 15:30:17 +0100

    fix(interval): update authentication.js

--- root/usr/lib/node/nethcti-server/plugins/authentication/authentication.js
+++ root/usr/lib/node/nethcti-server/plugins/authentication/authentication.js
@@ -218,9 +218,9 @@ var mapShibbolethUser = {};
  * @property expires
  * @type number
  * @private
- * @default 10 * 24 * 60 * 60 * 1000 (10 days)
+ * @default 10 * 24 * 60 * 60 (10 days)
  */
-var expires = 10 * 24 * 60 * 60 * 1000;
+var expires = 10 * 24 * 60 * 60;
 
 /**
  * If true, every authentication request also causes the update of the
